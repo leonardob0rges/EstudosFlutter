7a98fa10b56041dc0fc5f304aae4dae4c30f0793
Leonardo Borges <leonardoaborges13@gmail.com>
2023-03-05 23:13:46 -0300

    atualizando app

--- modulo1/meuapp/lib/main.dart
+++ modulo1/meuapp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Meu App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'App Flutter'),
     );
   }
 }
@@ -96,11 +96,15 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você clicou no botão:',
+              style: TextStyle(color: Colors.blue, fontSize: 30),
+              textAlign: TextAlign.center,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '$_counter vezes',
+              style: Theme.of(context).textTheme.displayMedium,
+              textAlign: TextAlign.center,
+              
             ),
           ],
         ),
